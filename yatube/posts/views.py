8e661fa359188c8ece3a848f6a967d9218ebaf6a
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-08-28 17:13:33 +0300

    ф

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -83,6 +83,6 @@ def post_edit(request, post_id):
             post = form.save()
             if post.author == request.user:
                 post.save()
-                return redirect('posts:post_detail', post_id=post_id)
-    form = PostForm()
+                return redirect('posts:post_detail', post_id=post_id, is_edit=True)
+    form = PostForm(instance=post)
     return render(request, 'posts/create_post.html', {'form': form})
