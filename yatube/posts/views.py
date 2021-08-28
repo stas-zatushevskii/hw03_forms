3876ea0efb0d7907e98fc4be5ccb291b62341c75
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-08-28 17:57:14 +0300

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -76,8 +76,8 @@ def post_create(request):
 
 @login_required
 def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
-        post = get_object_or_404(Post, id=post_id)
         form = PostForm(request.POST or None, instance=post)
         if form.is_valid():
             post = form.save()
